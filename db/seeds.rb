1936b8bb0fbfa1982e99167cace9ddf811f72688
maxime vanderwees <vanderwees.maxime@gmail.com>
2020-02-12 16:22:30 +0100

    finished core functions yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'launching seeds'
+puts 'destroying previous db'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'creating restaurants'
+Restaurant.create!(name: "pizza nico", address: "balguery", phone_number: "06 06 06 06 06", category: "italian")
+Restaurant.create!(name: "pizza lico", address: "balguery", phone_number: "07 07 07 07 07", category: "italian")
+Restaurant.create!(name: "pizza mico", address: "balguery", phone_number: "08 08 08 08 08", category: "italian")
+Restaurant.create!(name: "pizza dico", address: "balguery", phone_number: "06 06 06 06 06", category: "italian")
+puts 'creating reviews'
+Review.create!(restaurant_id: 1, content: "it was amazing", rating: 5)
+
+puts 'everything has been added gracefully'
